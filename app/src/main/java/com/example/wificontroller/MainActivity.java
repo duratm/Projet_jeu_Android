f3b95851b76aac8ea67c74149baf5525bed93505
duratm <mathias.durat@etu.univ-grenoble-alpes.fr>
2023-01-25 20:39:53 +0100

    joystick opérationnel

--- app/src/main/java/com/example/wificontroller/MainActivity.java
+++ app/src/main/java/com/example/wificontroller/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.wificontroller;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.FrameLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void control(View view){
+        Intent intent = new Intent(this, ControllerActivity.class);
+        startActivity(intent);
     }
 }
