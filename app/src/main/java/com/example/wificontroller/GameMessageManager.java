f16873e8917498415448f94e8868c4a7fe1fb701
mathias <duratmathias@gmail.com>
2023-02-22 09:14:56 +0100

    navigation en cours

--- app/src/main/java/com/example/wificontroller/GameMessageManager.java
+++ app/src/main/java/com/example/wificontroller/GameMessageManager.java
@@ -3,8 +3,6 @@ package com.example.wificontroller;
 import android.util.Log;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
