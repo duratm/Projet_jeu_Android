e366477116703c55a73a5c2ebb5e2090d5b64862
Quentin <quentin.farret@gmail.com>
2023-02-28 11:39:11 +0100

    :recycle: refactoring code et amelioratipn fonctionnalités

--- app/src/main/java/com/example/wificontroller/MainActivity.java
+++ app/src/main/java/com/example/wificontroller/MainActivity.java
@@ -24,6 +24,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,36 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         final Button button = findViewById(R.id.connect);
-        FileInputStream inputStream = null;
-        File file = new File(getFilesDir(), "server");
-        if (file.exists()) {
-            try {
-                inputStream = openFileInput("server");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
 
-            ArrayList<String> adresses = new ArrayList<String>();
-            try {
-                String content;
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                while ((content = bufferedReader.readLine()) != null) {
-                    adresses.add(content);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, adresses);
-            adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-            spinner.setAdapter(adapter);
-        }
+        ArrayList<String> adresses = lireFichier();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, adresses);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         TextView textFromInput = findViewById(R.id.enter);
 
         spinner.setOnItemSelectedListener(
                 new AdapterView.OnItemSelectedListener(){
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                        textFromInput.setText(spinner.getSelectedItem().toString());
+                        if ((adresses.size() == 1) || (spinner.getSelectedItem().toString().equals("Choisissez dans votre historique"))){
+                            textFromInput.setText("");
+                        }
+                        else {
+                            textFromInput.setText(spinner.getSelectedItem().toString());
+                        }
                     }
 
                     @Override
@@ -80,57 +69,7 @@ public class MainActivity extends AppCompatActivity {
                         String adressServer = textFromInput.getText().toString();
                         GameMessageManager.logActivity(true);
                         GameMessageManager.connect(adressServer);
-                        FileOutputStream outputStream = null;
-
-
-                        ArrayList<String> adresses = new ArrayList<String>();
-                        FileInputStream inputStream = null;
-                        File file = new File(getFilesDir(), "server");
-                        if (file.exists()) {
-                            try {
-                                inputStream = openFileInput("server");
-                            } catch (FileNotFoundException e) {
-                                throw new RuntimeException(e);
-                            }
-                            try {
-                                String content;
-                                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                                while ((content = bufferedReader.readLine()) != null) {
-                                    adresses.add(content);
-                                }
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-
-                        if (!file.exists()) {
-                            FileOutputStream fileOutputStream = null;
-                            try {
-                                fileOutputStream = openFileOutput("server", Context.MODE_PRIVATE);
-                                fileOutputStream.write((adressServer + System.getProperty("line.separator")).getBytes());
-                                fileOutputStream.close();
-                            } catch (FileNotFoundException e) {
-                                throw new RuntimeException(e);
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
-
-                        } else {
-                            if (!adresses.contains(adressServer)) {
-                                Log.i("adresses", adresses.toString());
-                                Log.i("add", adressServer);
-                                FileOutputStream fileOutputStream = null;
-                                try {
-                                    fileOutputStream = openFileOutput("server", Context.MODE_APPEND);
-                                    fileOutputStream.write((adressServer + System.getProperty("line.separator")).getBytes());
-                                    fileOutputStream.close();
-                                } catch (FileNotFoundException e) {
-                                    throw new RuntimeException(e);
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                            }
-                        }
+                        ecrireFichier(adressServer);
                         control(v);
                         //GameMessageManager.sendMessage("MOTL=0.5");
                     }
@@ -144,5 +83,66 @@ public class MainActivity extends AppCompatActivity {
     public void control(View view) {
         Intent intent = new Intent(this, FirstController.class);
         startActivity(intent);
+    }
+
+    public ArrayList<String> lireFichier(){
+        FileInputStream inputStream = null;
+        File file = new File(getFilesDir(), "test3");
+        ArrayList<String> adresses = new ArrayList<String>();
+        adresses.add(0, "Choisissez dans votre historique");
+        if (file.exists()) {
+            try {
+                inputStream = openFileInput("test3");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                String content;
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                while ((content = bufferedReader.readLine()) != null) {
+                    adresses.add(1,content);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return adresses;
+    }
+
+    public void ecrireFichier(String adressServer) {
+        File file = new File(getFilesDir(), "test3");
+        ArrayList<String> adresses = lireFichier();
+        FileOutputStream outputStream = null;
+        if (!Objects.equals(adressServer, "")) {
+            if (!file.exists()) {
+                FileOutputStream fileOutputStream = null;
+                try {
+                    fileOutputStream = openFileOutput("test3", Context.MODE_PRIVATE);
+                    fileOutputStream.write((adressServer + System.getProperty("line.separator")).getBytes());
+                    fileOutputStream.close();
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            } else {
+                if (!adresses.contains(adressServer)) {
+                    Log.i("adresses", adresses.toString());
+                    Log.i("add", adressServer);
+                    FileOutputStream fileOutputStream = null;
+                    try {
+                        fileOutputStream = openFileOutput("test3", Context.MODE_APPEND);
+                        fileOutputStream.write((adressServer + System.getProperty("line.separator")).getBytes());
+                        fileOutputStream.close();
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
     }
 }
